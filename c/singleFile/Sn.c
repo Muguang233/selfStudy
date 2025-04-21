b22980a7412a85c988c73576bea6018e414d49d0
Zellane <muguang233@gmail.com>
2025-04-22 06:32:07 +1000

    实现函数输出Sn方程式

---
+++ c/singleFile/Sn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int a = 2;
+  int n;
+  int term = a;
+  int sum = 0;
+  scanf(" %d", &n);
+  for (int i = 0; i < n; i++) {
+    sum += term;
+    term = term * 10 + a;
+  }
+  printf("%d\n", sum);
+  return 0;
+}
