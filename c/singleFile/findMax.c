528e7308b5c93cc1a9498feb3024d37920e8473a
Zellane <muguang233@gmail.com>
2025-04-21 15:28:30 +1000

    实现findMax 寻找三个数字中的最大值

---
+++ c/singleFile/findMax.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf(" %d %d %d", &a, &b, &c);
+    int curr_max = a;
+    if (b > curr_max) {
+      curr_max = b;
+    }
+    if (c > curr_max) {
+      curr_max = c;
+    }
+    printf("%d\n", curr_max);
+    return 0;
+}
