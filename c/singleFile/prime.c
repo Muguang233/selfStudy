1e5c1d731879824eaa8d7473d10a5d3c4e803394
Zellane <muguang233@gmail.com>
2025-05-08 12:50:53 +1000

    实现素数练习题

---
+++ c/singleFile/prime.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#define uwu int
+#define qvq {
+#define QoQ }
+#define AWA (
+#define awa )
+#define QAQ return
+#define QWQ 0
+#define qwq ;
+
+uwu main AWA awa qvq
+    uwu n qwq
+    scanf("%d", &n) qwq
+    uwu is_prime = 1 qwq
+    if AWA n <= 1 awa qvq
+        printf("not prime\n") qwq
+        QAQ QWQ qwq
+    QoQ
+    for AWA uwu i = 2 qwq i <= sqrt(n) qwq i++ awa qvq
+        if AWA n % i == 0 awa qvq
+            is_prime = 0 qwq
+            break qwq
+        QoQ
+    QoQ
+    if AWA is_prime awa qvq
+        printf("prime\n") qwq
+    QoQ else qvq
+        printf("not prime\n") qwq
+    QoQ
+    QAQ QWQ qwq
+QoQ
